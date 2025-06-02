84500c470a4558053d307828da9129b13d652b91
Rakshaa <97796804+rakshaa-01@users.noreply.github.com>
2025-06-02 20:12:24 +0530

    Time: 0 ms (100%), Space: 8.6 MB (48.96%) - LeetHub

---
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int rearrangeCharacters(string s, string target) {
+        if(target.length()>s.length()) {
+            return 0;
+        }
+
+        unordered_map<char, int> mp_s, mp_target;
+        
+        for(auto& ch: s) {
+            mp_s[ch]++;
+        }
+        for(auto& ch: target) {
+            mp_target[ch]++;
+        }
+        int ans=INT_MAX;
+        for(auto& ch: target) {
+            if(mp_s[ch]==0) return 0;
+            ans=min(ans, mp_s[ch]/mp_target[ch]);
+        }
+        return ans;
+    }
+};
