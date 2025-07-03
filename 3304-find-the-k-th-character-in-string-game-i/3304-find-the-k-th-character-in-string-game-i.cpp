45b508bc08d8d57c8faacc07f0530633f877cbb9
Rakshaa <97796804+rakshaa-01@users.noreply.github.com>
2025-07-03 20:40:36 +0530

    Time: 6 ms (21.87%), Space: 10.1 MB (41.81%) - LeetHub

---
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string s="a";
+        while(s.length()<=k) {
+            string temp="";
+            for(char c: s) {
+                if(c=='z') {
+                    temp.push_back('a');
+                }
+                temp.push_back(c+1);
+            }
+            s.append(temp);
+        }
+        return s[k-1];
+    }
+};
