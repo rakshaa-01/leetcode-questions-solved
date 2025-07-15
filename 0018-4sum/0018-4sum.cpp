0121c887806e98438c175b2494aadbb0344552ad
Rakshaa <97796804+rakshaa-01@users.noreply.github.com>
2025-07-16 00:50:19 +0530

    Time: 19 ms (81.87%), Space: 17.5 MB (58.3%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& arr, int target) {
-        set<vector<int>> st;
+        vector<vector<int>> ans;
         int n=arr.size();
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                set<long long> s;
-                for(int k=j+1; k<n; k++) {
-                    long long sum=arr[i]+arr[j];
-                    sum+=arr[k];
-                    long long more=target-sum;
-                    if(s.find(more)!=s.end()) {
-                        vector<int> temp={(int)arr[i], (int)arr[j], (int)arr[k], (int)more};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
-                    }
-                    s.insert(arr[k]);
-                }
-            }
-        }
-        vector<vector<int>> ans(st.begin(), st.end());
-        return ans;
+   sort(arr.begin(), arr.end());
+
+   for(int i=0; i<n; i++) {
+    if(i>0 && arr[i]==arr[i-1]) continue;
+    for(int j=i+1; j<n; j++) {
+        if(j>i+1 && arr[j]==arr[j-1]) continue;
+     int k=j+1, l=n-1;
+     while(k<l) {
+       long long sum = (long long)arr[i]+arr[j]+arr[k]+arr[l];
+       if(sum<target) {
+         k++;
+       }
+       else if(sum>target) {
+         l--;
+       }
+       else {
+         vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+         ans.push_back(temp);
+         k++;
+         l--;
+         while(k<l && arr[k]==arr[k-1]) k++;
+         while(k<l && arr[l]==arr[l+1]) l--;
+       }
+    }
+   }
+   }
+   return ans;
     }
 };
