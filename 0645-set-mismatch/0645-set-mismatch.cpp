9c7f20bab8775e2f26a12685df9f001a600ad43b
Rakshaa <97796804+rakshaa-01@users.noreply.github.com>
2025-11-12 17:15:57 +0530

    Time: 0 ms (100%), Space: 26.2 MB (45.48%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> ans;
-        //Find duplicate
-        unordered_set<int> s;
-        for(auto& n: nums) {
-            if(s.count(n)) {
-                ans.push_back(n);
-                break;
-            }
-            s.insert(n);
-        }
-
-        //Find the missing
-        int n=1;
+        vector<int> v(nums.size()+1, 0);
         for(auto& ch: nums) {
-            if(n!=ch) {
-                ans.push_back(n);
-                break;
-            }
-            n++;
+            v[ch]++;
         }
-        return ans;
+        int rep=-1, mis=-1;
+        for(int i=1; i<v.size(); i++) {
+            if(v[i]==2) rep=i;
+            else if(v[i]==0) mis=i;
+        }
+        return {rep, mis};
     }
 };
