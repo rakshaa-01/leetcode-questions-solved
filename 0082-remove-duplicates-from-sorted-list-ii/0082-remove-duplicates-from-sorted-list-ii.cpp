8177d827f445f993999d6bc8d3dc11773d873263
Rakshaa <97796804+rakshaa-01@users.noreply.github.com>
2025-11-18 09:44:04 +0530

    Time: 0 ms (100%), Space: 15.7 MB (71.55%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -1,33 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head == NULL || head->next == NULL) return head;
-
-        ListNode* dummy = new ListNode(INT_MIN);
-        dummy->next = head;
-
-        ListNode* curr = dummy;
-        ListNode* prev;
-        ListNode* succ;
-
-        while (curr->next) {
-            prev = curr->next;
-            succ = prev->next;
-
-            bool duplicate = false;
-
-            while (succ && succ->val == prev->val) {
-                duplicate = true;
-                succ = succ->next;
+        ListNode* dummy=new ListNode(INT_MIN);
+        dummy->next=head;
+        ListNode* curr=dummy->next;
+        ListNode* prev=dummy;
+        while(curr) {
+            if(curr->next!=NULL && curr->val==curr->next->val) {
+                int val=curr->val;
+                while(curr!=NULL && curr->val==val) 
+                    curr=curr->next;
+                prev->next=curr;
             }
-
-            if (duplicate) {
-                curr->next = succ;
-            } else {
-                curr = curr->next;
+            else {
+                prev=curr;
+                curr=curr->next;
             }
         }
-
         return dummy->next;
     }
 };
